125e2f98576dee52d09df955b72024907306fac1
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-01 18:07:45 +0000

    Update TGW name, clean up model.

--- tgw.tf
+++ tgw.tf
@@ -21,8 +21,8 @@ module "tgw" {
   source  = "terraform-aws-modules/transit-gateway/aws"
   version = "~> 2.0"
 
-  name        = "my-tgw"
-  description = "My TGW shared with several other AWS accounts"
+  name        = "Shared-TGW"
+  description = "TGW shared with several other AWS accounts"
 
   enable_auto_accept_shared_attachments = true
 
@@ -74,20 +74,3 @@ module "vpc" {
 	Project = "Azure-AWS"
   }
 }
-
-
-# module "vpc" {
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "~> 3.0"
-# 
-#   name = "my-vpc"
-# 
-#   cidr = "10.10.0.0/16"
-# 
-#   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-#   private_subnets = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
-# 
-#   enable_ipv6                                    = true
-#   private_subnet_assign_ipv6_address_on_creation = true
-#   private_subnet_ipv6_prefixes                   = [0, 1, 2]
-# }
