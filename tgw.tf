3b96f57c351543f309a37b2aca946c0d4859f12f
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-01 21:11:15 +0000

    Update name into locals section

--- tgw.tf
+++ tgw.tf
@@ -13,6 +13,7 @@ provider "aws" {
 }
 
 locals {
+  name   = "Shared-TGW"
   region = "eu-west-1"
   #region = var.region
 }
@@ -21,7 +22,7 @@ module "tgw" {
   source  = "terraform-aws-modules/transit-gateway/aws"
   version = "~> 2.0"
 
-  name        = "Shared-TGW"
+  name        = local.name
   description = "TGW shared with several other AWS accounts"
 
   enable_auto_accept_shared_attachments = true
