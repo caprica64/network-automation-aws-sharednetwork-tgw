14bca849fce14b222bb54238d2febe9af9b3677e
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-05 21:52:31 +0000

    resource "aws_ec2_transit_gateway_route_table" "spoke1" {   transit_gateway_id = aws_ec2_transit_gateway.hub.id }

--- main.tf
+++ main.tf
@@ -267,6 +267,10 @@ resource "aws_ec2_transit_gateway" "hub" {
   }
 }
 
+resource "aws_ec2_transit_gateway_route_table" "spoke1" {
+  transit_gateway_id = aws_ec2_transit_gateway.hub.id
+}
+
 ## Vars 
 ## vpc name
 ## subnet count
