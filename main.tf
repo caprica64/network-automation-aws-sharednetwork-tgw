6698fff814299d8fd72728490914dc91748d7642
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-06 17:11:45 +0000

    Fixed tgw ref in >
    route {
    	  cidr_block = "10.1.0.0/16"
    	  transit_gateway_id = aws_ec2_transit_gateway.hub.id
      }

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "aws_route_table" "RouteTablePublic" {
 
   route {
 	  cidr_block = "10.1.0.0/16"
-	  transit_gateway_id = aws_ec2_transit_gateway.hub.id
+	  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   }
 }
 
