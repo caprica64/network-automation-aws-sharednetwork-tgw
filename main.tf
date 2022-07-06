f7707f1748589c76363c67b522c47ea5a2b9fe29
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-06 16:37:43 +0000

    default_route_table_association = "enable"

--- main.tf
+++ main.tf
@@ -207,7 +207,7 @@ resource "aws_ec2_transit_gateway" "tgw" {
   amazon_side_asn = 64512
   auto_accept_shared_attachments = "enable"
   dns_support = "enable"
-  default_route_table_association = "disable"
+  default_route_table_association = "enable"
   default_route_table_propagation = "disable"
   vpn_ecmp_support = "enable"
   
