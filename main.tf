675964f74ac27641921c552530d568fee8fcd0b3
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-06 16:43:44 +0000

    ################################################################################ # VPC Attachment section ################################################################################

--- main.tf
+++ main.tf
@@ -246,21 +246,21 @@ resource "aws_ram_principal_association" "main-org" {
 ################################################################################
 # VPC Attachment section
 ################################################################################
-# resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_vpc_attach-public-subnets" {
-#   subnet_ids         = [aws_subnet.PublicSubnet1a.id, aws_subnet.PublicSubnet1c.id]
-#   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-#   vpc_id             = aws_vpc.VPC.id
-#   
-#   appliance_mode_support = "disable"
-#   dns_support = "enable"
-#   #ipv6_support = "enable"
-#   transit_gateway_default_route_table_association = true
-#   transit_gateway_default_route_table_propagation = true
-# 
-#   tags = {
-# 	Name = "Public-subnet-attachment"
-#   }
-# }
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_vpc_attach-public-subnets" {
+  subnet_ids         = [aws_subnet.PublicSubnet1a.id, aws_subnet.PublicSubnet1c.id]
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  vpc_id             = aws_vpc.VPC.id
+  
+  appliance_mode_support = "disable"
+  dns_support = "enable"
+  #ipv6_support = "enable"
+  transit_gateway_default_route_table_association = true
+  transit_gateway_default_route_table_propagation = true
+
+  tags = {
+	Name = "Public-subnet-attachment"
+  }
+}
 
 ################################################################################
 # Transit Gateway routing table
