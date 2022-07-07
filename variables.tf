0249563f4fb39f15828b9faa43600a529d950c76
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-07 15:02:43 +0000

    Changed var private to correct name public

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 #VPC variables
 variable "vpc_name" {}
 variable "cidr_block" {}
-variable "private_subnet_1a_cidr" {}
-variable "private_subnet_1c_cidr" {}
+variable "public_subnet_1a_cidr" {}
+variable "public_subnet_1c_cidr" {}
 
 #Spoke 1 destination CIDR block
 variable "spoke1_cidr" {}
